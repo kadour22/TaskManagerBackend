456e0902573ced49143538cc9076e93f11401a30
kadour <kadourzaroui399@gmail.com>
2025-10-15 06:58:13 -0400

    integrate celery

--- backend/celery.py
+++ backend/celery.py
@@ -2,16 +2,11 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-# Set default Django settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-# Create Celery instance
+
 app = Celery('backend')
-
-# Load settings from Django settings with a CELERY_ prefix
 app.config_from_object('django.conf:settings', namespace='CELERY')
-
-# Auto-discover tasks from all installed apps
 app.autodiscover_tasks()
 
 @app.task(bind=True)

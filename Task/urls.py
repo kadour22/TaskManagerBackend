cadbd5eec4ad4b4658aaaf24a535eaa855ae7511
kadour <kadourzaroui399@gmail.com>
2025-10-16 07:31:54 -0400

    add obtain and refresh tokens enspoints

--- Task/urls.py
+++ Task/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('tasks/', views.TaskListCreateView.as_view(), name='task-list-create'),
     path('tasks/<int:pk>/', views.TaskDetailView.as_view(), name='task-list-create'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]

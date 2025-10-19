5e4ed91f2262e141a6d4516d14b6955b54a9270f
kadour <kadourzaroui399@gmail.com>
2025-10-19 08:15:29 -0400

    install and configure cors headers

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Task',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -39,10 +40,14 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "allauth.account.middleware.AccountMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = 'backend.urls'
-
+CORS_ALLOWED_ORIGINS = [
+    'https://taskmanagerbackend-1-fnmh.onrender.com'
+]
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',

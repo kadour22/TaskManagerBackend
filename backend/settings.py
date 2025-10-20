ac187dfdf502964015f5a0cc1d1297348db1ae24
kadour <kadourzaroui399@gmail.com>
2025-10-20 05:51:13 -0400

    add frontend url to allowed origins

--- backend/settings.py
+++ backend/settings.py
@@ -32,6 +32,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -40,13 +42,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "allauth.account.middleware.AccountMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
-    "django.middleware.common.CommonMiddleware",
+
 ]
 
 ROOT_URLCONF = 'backend.urls'
 CORS_ALLOWED_ORIGINS = [
-    'https://taskmanagerbackend-1-fnmh.onrender.com'
+    "http://localhost:5173",  # your React app
 ]
 TEMPLATES = [
     {

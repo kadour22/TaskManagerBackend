00ff22211a7f72cfee4bd435d5db44d90f60398c
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-10-28 20:44:27 +0100

    fixe post request

--- Task/views.py
+++ Task/views.py
@@ -15,7 +15,7 @@ class task_list_create(APIView) :
     def post(self, request):
         serializer = TaskSerializer(data=request.data)
         if serializer.is_valid() :
-            serializer.save()
+            serializer.save(user=request.user)
             return Response(serializer.data , status=status.HTTP_201_CREATED)
         return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
 

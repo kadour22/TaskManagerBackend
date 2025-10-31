09f0125eeb489a27f01221889cfe47e7e69f7892
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-10-31 22:47:39 +0100

    add mark as completed the task view

--- Task/views.py
+++ Task/views.py
@@ -1,5 +1,6 @@
 
 from django.conf import settings
+from django.shortcuts import get_object_or_404
 # rest framework imports
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -8,8 +9,6 @@ from rest_framework.permissions import IsAuthenticated
 # local imports 
 from .serializers import TaskSerializer
 from .models import Task
-from .services.services import task_list, create_task, update_task, delete_task , get_task, completed_tasks_list
-
 
 class task_list_create(APIView) :
     permission_classes = [IsAuthenticated]
@@ -24,3 +23,13 @@ class task_list_create(APIView) :
         tasks = Task.objects.select_related("user").all()
         serializer = TaskSerializer(tasks , many=True)
         return Response(serializer.data , status=status.HTTP_200_OK)
+
+
+class mark_task_as_completed(APIView):
+    permission_classes= [IsAuthenticated]
+    def update(self,request,task_id) :
+        task = get_object_or_404(Task,id=task_id)
+        task.completed = True
+        task.save()
+        return Response("task updated")
+        

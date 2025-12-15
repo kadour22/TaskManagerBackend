e36a52e4bad8fc4e62afbae9ce5f8df96329f17c
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-12-15 23:38:34 +0100

    add create task  service to view

--- Task/views.py
+++ Task/views.py
@@ -14,11 +14,7 @@ from .services.services import tasks_list , create_task , mark_task_as_completed
 class task_list_create(APIView) :
     permission_classes = [IsAuthenticated]
     def post(self, request):
-        serializer = TaskSerializer(data=request.data)
-        if serializer.is_valid() :
-            serializer.save(user=request.user)
-            return Response(serializer.data , status=status.HTTP_201_CREATED)
-        return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
+        return create_task(data=request.data,user=request.user)
 
     def get(self,request):
         return tasks_list(user=request.user)
